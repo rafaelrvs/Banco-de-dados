fdf48e2c5cb7f7523b616e7193d5ee884b955b71
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-11 21:36:22 -0300

    Update Tabela1.sql

--- Tabela1.sql
+++ Tabela1.sql
@@ -4,3 +4,24 @@ create table cliente(
 	endereco varchar (200) not null,
 	constraint pk_cli primary key (numero)
 );
+
+create table carro(
+	registro integer,
+	marca  varchar(150) not null,
+	modelo  varchar(150) not null,
+	cor varchar(150) not null,
+	constraint pk_carro primary key (registro)
+	
+);
+create table apolice (
+	numero integer,
+	valor numeric(10,2) not null check (valor>0),
+	data_inicio date not null,
+	data_fim date not null,
+	num_cliente integer not null,
+	reg_carro integer not null,
+	constraint pk_apolice primary key (numero),
+	constraint pk_apolice_cli foreign key(num_cliente) references cliente(numero),
+	constraint fk_apolice_car foreign key (reg_carro)references carro(registro)
+);
+
