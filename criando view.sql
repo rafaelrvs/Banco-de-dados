9210df1c332f27347b88023c202811322ea6b353
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-12 03:14:17 -0300

    Update criando view.sql

--- criando view.sql
+++ criando view.sql
@@ -23,6 +23,18 @@ select c.registro, c.marca, c.modelo, c.cor
 from carro c
 where c.registro =10
 
+select c.registro, c.marca, c.modelo, c.cor
+from carro c
+where c.registro =10
+
+--4. Escreva o comando SQL que exiba o número de cada apólice, o nome do cliente e a 
+--marca do veículo segurado, das apólices com início no mês de abril de 2023. O resultado deve 
+--estar ordenado em ordem crescente pelo nome do cliente. 
+
+select c.nome, a.numero, a.data_inicio
+from apolice a, cliente c
+where c.numero = a.numero and a.data_inicio between '2023-04-01 ' and '2023-04-30' 
+
 
 
 
