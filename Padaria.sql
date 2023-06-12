ae5d998e14f18742e97cd03cacf9af0d01e8422e
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-12 14:17:32 -0300

    Update Padaria.sql

--- Padaria.sql
+++ Padaria.sql
@@ -16,13 +16,49 @@ create table valores(
 	nome_produto integer not null,
 	constraint fk_produto_nome foreign key(nome_produto) references produto(id)
 );
+alter table valores alter column nome_produto drop not null
 
+
+create table auditoria_padaria(
+	codigo_pad integer,
+	data_alteracao date not null
+);
+
+
+
+
+create trigger log_padaria
+after insert or UPDATE ON produto
+for each row
+execute procedure auditoria_log_padaria();
+
+
+create or replace function auditoria_log_padaria()
+RETURNS trigger AS 
+$$
+BEGIN
+insert into auditoria_padaria (codigo_pad, data_alteracao)
+values
+(NEW.codigo, current_date);
+return NEW;
+END;
+$$ 	language 'plpgsql';
+
+
+select*
+from valores 
+
+update produto set nome = 'batata'
 
 insert into produto values (1,'pao')
 insert into produto values (2,'queijo')
+insert into produto values (3,'salame')
+
+
+
 insert into valores values (01,2.50,1)
 insert into valores values (02,5.50,2)
-
+insert into valores values (03,10.50)
 
 
 select*
