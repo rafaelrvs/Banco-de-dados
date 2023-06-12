79f089999f54c59742b1f363cefffac44bae43f6
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-12 02:53:10 -0300

    Update criando view.sql

--- criando view.sql
+++ criando view.sql
@@ -4,3 +4,26 @@ from carro
 
 select *
 from cliente_seguro
+
+
+create view cliente_apolice  as
+select*
+from apolice
+
+create view cliente_apolice_aci as
+select *
+from acidente
+
+select c.local, c.hora
+from cliente_apolice_aci c
+where c.local ilike 'rua branca'
+
+
+select c.registro, c.marca, c.modelo, c.cor
+from carro c
+where c.registro =10
+
+
+
+
+
